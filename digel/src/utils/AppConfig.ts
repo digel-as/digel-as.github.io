25e19be3355981efbee93b04bcbe8d6cfaea26e5
Thomas Andersen <thom.as.and@hotmail.com>
2024-04-03 14:24:45 +0200

    digel metadata

--- digel/src/utils/AppConfig.ts
+++ digel/src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'React landing page',
-  title: 'React landing page template 2021',
-  description: 'Production ready plug n play landing page!',
-  locale: 'en',
+  site_name: "digel",
+  title: "digel",
+  description: "The home of Digel",
+  locale: "en",
 };
