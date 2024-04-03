9561367e54da8c45162cd19754dbd61529dce708
Thomas Andersen <thom.as.and@hotmail.com>
2024-04-03 20:59:08 +0200

    change font color

--- digel/src/components/Introduction.tsx
+++ digel/src/components/Introduction.tsx
@@ -21,7 +21,7 @@ const Introduction = () => {
           >
             <div className="w-full sm:w-1/2 p-6">
               <div className="align-middle">
-                <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3 text-tertiary">
+                <h3 className="text-3xl font-bold leading-none mb-3 text-tertiary">
                   {item.title}
                 </h3>
                 <p className="text-white mb-8">{item.description}</p>
