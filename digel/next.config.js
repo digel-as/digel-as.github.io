c9c3966254894b7777ee251629a51545a8c1440a
Thomas Andersen <thom.as.and@hotmail.com>
2024-04-04 11:42:16 +0200

    edit for github pages

--- digel/next.config.js
+++ digel/next.config.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({
+  basePath: "digel",
+  output: "export", // Enables static exports
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
+  basePath: "",
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
