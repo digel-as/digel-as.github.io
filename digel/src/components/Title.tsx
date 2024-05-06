c4e971fa5629204783c80478a1524299c5aca259
Thomas Andersen <thom.as.and@hotmail.com>
2024-05-06 22:27:05 +0200

    gaps + text align left for larger screens

--- digel/src/components/Title.tsx
+++ digel/src/components/Title.tsx
@@ -6,13 +6,13 @@ const Title = () => {
   const { title } = config;
   return (
     <main className="flex items-center justify-center h-screen bg-backgroundDark">
-      <div className="max-w-screen-xl text-center flex flex-col md:flex-row items-center mx-16 xl:gap-11">
+      <div className="max-w-screen-xl text-center flex flex-col md:flex-row items-center mx-16 md:gap-8 lg:gap-12">
         <img
           src={title.img}
           alt="Title"
           className="mx-auto mb-4 md:mb-0 md:mr-4"
         />
-        <h1 className="sm:text-xl md:text-2xl lg:text-4xl xl:text-6xl text-primary xl:text-left">
+        <h1 className="sm:text-xl md:text-2xl lg:text-4xl xl:text-6xl text-primary md:text-left">
           {title.slogan}
         </h1>
       </div>
