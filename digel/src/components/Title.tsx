1d0509f2a75193fe610bcaae353971b2e6b24c06
Thomas Andersen <thom.as.and@hotmail.com>
2024-05-06 23:09:48 +0200

    add bg image

--- digel/src/components/Title.tsx
+++ digel/src/components/Title.tsx
@@ -6,6 +6,14 @@ const Title = () => {
   const { title } = config;
   return (
     <main className="flex items-center justify-center h-screen bg-backgroundDark">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: "url('/assets/images/industrial-plant.jpg')",
+          opacity: 1, // Adjust opacity as needed to blend with the background color
+          mixBlendMode: "soft-light", // Change blend mode as needed to achieve the desired effect
+        }}
+      ></div>
       <div className="max-w-screen-xl text-center flex flex-col md:flex-row items-center mx-16 md:gap-8 lg:gap-12">
         <img
           src={title.img}
